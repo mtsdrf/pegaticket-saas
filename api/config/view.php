2130a4f31fa182161d116cde10f67d6d91667eb4
Mateus Ferreira <mreisf.contato@gmail.com>
2026-08-07 11:39:58 -0300

    Ajustes

---
+++ api/config/view.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Blade e demais engines de view procuram templates nesses caminhos.
+    | O projeto segue o padrão Laravel em `resources/views`.
+    |
+    */
+
+    'paths' => [
+        resource_path('views'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Compiled View Path
+    |--------------------------------------------------------------------------
+    |
+    | O CI limpa o cache de configuração antes dos testes; sem esse arquivo,
+    | `view.compiled` fica nulo e qualquer renderização Blade falha com
+    | "Please provide a valid cache path.".
+    |
+    */
+
+    'compiled' => env(
+        'VIEW_COMPILED_PATH',
+        realpath(storage_path('framework/views'))
+    ),
+
+];
