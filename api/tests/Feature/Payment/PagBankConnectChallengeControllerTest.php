dfae034e69ed420d8fd9b846b86ee8b8d22cad7d
Mateus Ferreira <mreisf.contato@gmail.com>
2026-08-08 17:25:07 -0300

    Ajuste pix

--- api/tests/Feature/Payment/PagBankConnectChallengeControllerTest.php
+++ api/tests/Feature/Payment/PagBankConnectChallengeControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Payment;
 
+use Illuminate\Support\Facades\Config;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -15,6 +16,25 @@ class PagBankConnectChallengeControllerTest extends TestCase
     #[Test]
     public function public_key_endpoint_returns_the_derived_public_key_without_authentication(): void
     {
+        $keyResource = openssl_pkey_new([
+            'private_key_type' => OPENSSL_KEYTYPE_RSA,
+            'private_key_bits' => 2048,
+        ]);
+
+        $this->assertNotFalse($keyResource);
+
+        $privateKeyPem = '';
+        $this->assertTrue(openssl_pkey_export($keyResource, $privateKeyPem));
+
+        $privateKeyPath = storage_path('framework/testing/pagbank-connect-challenge-test.key');
+        $directory = dirname($privateKeyPath);
+        if (! is_dir($directory)) {
+            mkdir($directory, 0777, true);
+        }
+
+        file_put_contents($privateKeyPath, $privateKeyPem);
+        Config::set('services.pagbank.connect_challenge_private_key_path', $privateKeyPath);
+
         $response = $this->getJson('/api/v1/pagbank-connect/public-key')
             ->assertStatus(200)
             ->assertJsonStructure(['public_key', 'created_at']);
@@ -23,5 +43,7 @@ class PagBankConnectChallengeControllerTest extends TestCase
 
         $this->assertStringContainsString('BEGIN PUBLIC KEY', $publicKey);
         $this->assertStringNotContainsString('PRIVATE KEY', $publicKey);
+
+        @unlink($privateKeyPath);
     }
 }
